61851ba1ebe1cd3e6eee735dde1fdeea27eeb7ba
Reilly <reillyodie@gmail.com>
2022-03-22 22:47:39 -0400

    Setup prisma with sqlite

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from 'remix';
+
+import stylesUrl from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }];
+};
+
 export default function HomeIndex() {
   return <span>Hello from index route</span>;
 }

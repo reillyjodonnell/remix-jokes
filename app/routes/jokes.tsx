52f00490977dd37038ba42db2124bcb38499b68e
Reilly <reillyodie@gmail.com>
2022-03-27 23:54:03 -0400

    Continued tut

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,12 +1,52 @@
-import { Outlet } from 'remix';
+import type { LinksFunction, LoaderFunction } from 'remix';
+import { json, Link, Outlet, useLoaderData } from 'remix';
+import { db } from '~/utils/db.server';
 
+type LoaderData = {
+  jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListItems: await db.joke.findMany(),
+  };
+  return json(data);
+};
 export default function JokesRoute() {
+  const data = useLoaderData<LoaderData>();
   return (
-    <>
-      <span>Jokes😂😂</span>
-      <main>
-        <Outlet />
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
       </main>
-    </>
+    </div>
   );
 }
